636770fda00ecae63051222fc2488e188cc0efd4
Emmanuel Rivera <129634455+emmannem@users.noreply.github.com>
2023-09-05 14:03:11 -0600

    Header Closes #3 (#4)
    * commit header #3
    
    * fix test #3
    
    * fix test #3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,12 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { WorkExperienceComponent } from './work-experience/work-experience.component';
 
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    WorkExperienceComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, WorkExperienceComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
