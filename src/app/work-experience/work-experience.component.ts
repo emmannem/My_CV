a4c87b6e9862d8f61c16dec2af0bfd6268fe7c86
Joseemmanuel <emmanuelriv15@gmail.com>
2023-09-08 14:22:14 -0600

    Create work-experience component

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WorkExperienceService } from '../services/work-experience.service';
 
 @Component({
   selector: 'app-work-experience',
   templateUrl: './work-experience.component.html',
-  styleUrls: ['./work-experience.component.css']
+  styleUrls: ['./work-experience.component.css'],
 })
 export class WorkExperienceComponent {
-
+  constructor(private workExoerienceService: WorkExperienceService) {}
 }
