7b2bccb9c94a43be2a93e0a76f9f0d13387ee7b4
Emmanuel Rivera <129634455+emmannem@users.noreply.github.com>
2023-09-15 13:22:25 -0600

    Header Finished #3 (#17)
    * commit header #3
    
    * fix test #3
    
    * fix test #3
    
    * Commit Header #3
    
    * Fixing changes in Header #3
    
    * Fixing changes in Header #3
    
    * Modified Header #3
    
    * Modified Header #3
    
    * Modified Header #3
    
    * Update master_CI_CD.yml (#12)
    
    * Modified Header #3
    
    * modified Header #3
    
    * modified Header #3

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -1,19 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { WorkExperienceService } from '../services/work-experience.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-work-experience',
   templateUrl: './work-experience.component.html',
   styleUrls: ['./work-experience.component.css'],
 })
-export class WorkExperienceComponent implements OnInit {
-  workInfo: Array<any> = [];
-
-  constructor(private workExperienceService: WorkExperienceService) {}
-
-  ngOnInit(): void {
-    this.workExperienceService.getWorkExperience().subscribe((data: any) => {
-      this.workInfo = data;
-    });
-  }
+export class WorkExperienceComponent {
+  constructor() {}
 }
